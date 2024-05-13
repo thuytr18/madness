009545b31c513e072a0ae4e3ff037a7e8f0bcc96
thuytr18 <thuytruong112@gmail.com>
2024-05-13 15:17:10 +0200

    adding Ba_programm

---
+++ src/examples/1ba_thesis_programm.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <madness/mra/funcdefaults.h>
+#include <madness/mra/function_factory.h>
+#include <madness/mra/legendre.h>
+#include <madness/mra/mra.h>
+#include <madness/mra/funcplot.h>
+#include <madness/mra/nonlinsol.h>
+#include <madness/mra/operator.h>
+#include <madness/tensor/gentensor.h>
+#include <madness/world/world.h>
+#include <madness/world/worldmpi.h>
+#include <madness/tensor/tensor.h>
+#include <vector>
+
+using namespace madness;
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+}
