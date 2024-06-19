96f77a4c9d9886eac94d7748bc05673b862ddf35
Thuy <thuytruong112@gmail.com>
2024-06-19 16:26:53 +0200

    class eigensolver

---
+++ src/bachelorthesis/eigensolver.h
@@ -0,0 +1,267 @@
+#ifndef EIGENSOLVER_H
+#define EIGENSOLVER_H
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <madness/mra/mra.h>
+#include <madness/mra/function_interface.h>
+#include <madness/mra/nonlinsol.h>
+#include <madness/mra/vmra.h>
+#include <madness/tensor/tensor.h>
+#include <madness/tensor/tensor_lapack.h>
+#include <madness/world/vector.h>
+#include <madness/world/world.h>
+#include <ostream>
+#include <vector>
+#include "guesses.h"
+#include "plot.h"
+
+using namespace madness;
+
+// Class to create Guesses, diagonalize, optimize with BSH Operator and diagonalize again
+// Input: World, Function (Potential), max Iteration, number of guesses or World, Function (Potential), max Iteration, number of guesses, Vector of Function (Guesses)
+// Output: Vector of Function (Diagonalized Eigenfunctions)
+
+template <typename T, std::size_t NDIM>
+class Eigensolver {
+    public:     
+        explicit Eigensolver(World& world) : world(world) {}
+
+        // Function to solve the eigenvalue problem for the given potential
+        std::vector<Function<double, NDIM>> solve(Function<T, NDIM>& V, int num_levels, int max_iter) {
+            // Create the guess generator
+            GuessGenerator<double, NDIM> guess_generator(world);              // Guess generator for all potentials
+            // Create the guess functions
+            std::vector<Function<double,NDIM>> guesses = guess_generator.create_guesses(num_levels, V);
+
+            // plot guess functions
+            for (int i = 0; i < guesses.size(); i++) {
+                char filename[512];
+                snprintf(filename, 512, "g-%1d.dat", i);
+                if (NDIM == 1)
+                    plot1D(filename, guesses[i]);
+                else if (NDIM == 2)
+                    plot2D(filename, guesses[i]);
+            }
+
+            // Diagonalize the Hamiltonian matrix
+            std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> tmp = diagonalize(world, guesses, V);
+            std::vector<Function<double, NDIM>> diagonalized_guesses = tmp.second;
+
+            // store the eigenfunctions in vector eigenfunctions
+            std::vector<Function<double, NDIM>> eigenfunctions;
+
+            // Generate and solve the diagonalized guesses and store them in eigenfunctions (Optimization with BSH Operator)
+            for (int i = 0; i < num_levels; i++) {
+                Function<double, NDIM> phi = generate_and_solve(world, V, diagonalized_guesses[i], i, eigenfunctions, max_iter);
+                eigenfunctions.push_back(phi);
+            }
+
+            // diagonalize the eigenfunctions again
+            std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> diagonalized = diagonalize(world, eigenfunctions, V);
+            std::cout << "Diagonalize" << std::endl;
+            std::vector<Function<double, NDIM>> y = diagonalized.second;
+
+            return y;
+        }
+
+        // Function to solve the eigenvalue problem for the given potential with given guesses
+        std::vector<Function<double, NDIM>> solve(Function<T, NDIM>& V, const std::vector<Function<T, NDIM>>& guesses, int num_levels, int max_iter) {
+            // Diagonalize the Hamiltonian matrix
+            std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> tmp = diagonalize(world, guesses, V);
+            std::vector<Function<double, NDIM>> diagonalized_guesses = tmp.second;
+
+            // store the eigenfunctions in vector eigenfunctions
+            std::vector<Function<double, NDIM>> eigenfunctions;
+
+            // Generate and solve the diagonalized guesses and store them in eigenfunctions (Optimization with BSH Operator)
+            for (int i = 0; i < num_levels; i++) {
+                Function<double, NDIM> phi = generate_and_solve(world, V, diagonalized_guesses[i], i, eigenfunctions, max_iter);
+                eigenfunctions.push_back(phi);
+            }
+
+            // diagonalize the eigenfunctions again
+            std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> diagonalized = diagonalize(world, eigenfunctions, V);
+            std::cout << "Diagonalize" << std::endl;
+            std::vector<Function<double, NDIM>> y = diagonalized.second;
+
+            return y;
+
+            // plot the eigenfunctions
+            for (int i = 0; i < num_levels; i++) {
+                char filename[256];
+                snprintf(filename, 256, "Psi_%1d.dat", i);
+                double en = energy(world, y[i], V);
+                std::cout << "Energy: " << en << std::endl;
+
+                // for hamonic oscillator
+                // V(r) = 0.5 * r * r = E
+                // inverse function: r = sqrt(2 * E)
+                /*
+                if(y[i](std::sqrt(2* en)) < 0) {
+                    y[i] *= -1;
+                }
+                */
+                
+                if (NDIM == 1)
+                // for harmonic oscillator: 0.75 * y[i] + en for optical reasons 
+                // before it was: y[i] + DELTA + en
+                    plot1D(filename, y[i] + en); 
+                else if (NDIM == 2)
+                    plot2D(filename, y[i]);
+            }
+        } 
+
+        // Function to calculate the energy
+        double energy(World& world, const Function<T, NDIM>& phi, const Function<T, NDIM>& V) {
+            double potential_energy = inner(phi, V * phi); // <phi|Vphi> = <phi|V|phi>
+            double kinetic_energy = 0.0;
+
+            Derivative<T, NDIM> D = free_space_derivative<T, NDIM>(world, 0); // Derivative operator
+
+            Function<T, NDIM> dphi = D(phi);
+            kinetic_energy += 0.5 * inner(dphi, dphi);  // (1/2) <dphi/dx | dphi/dx>
+
+            double energy = kinetic_energy + potential_energy;
+            return energy;
+        }
+
+    private:
+        World& world;
+
+        // Function to calculate the Hamiltonian matrix, Overlap matrix and Diagonal matrix
+        std::pair<Tensor<T>, std::vector<Function<T, NDIM>>> diagonalize(World &world, const std::vector<Function<T, NDIM>>& functions, const Function<T, NDIM>& V){
+            const int num = functions.size();
+            
+            auto H = Tensor<T>(num, num); // Hamiltonian matrix
+            auto overlap = Tensor<T>(num, num); // Overlap matrix
+            auto diag_matrix = Tensor<T>(num, num); // Diagonal matrix
+
+            // Calculate the Hamiltonian matrix
+            
+            for(int i = 0; i < num; i++) {
+                auto energy1 = energy(world, functions[i], V);
+                std::cout << energy1 << std::endl;
+                for(int j = 0; j < num; j++) {
+                    double kin_energy = 0.0;
+                    for (int axis = 0; axis < NDIM; axis++) {
+                        Derivative<T, NDIM> D = free_space_derivative<T,NDIM>(world, axis); // Derivative operator
+
+                        Function<T, NDIM> dx_i = D(functions[i]);
+                        Function<T, NDIM> dx_j = D(functions[j]);
+
+                        kin_energy += 0.5 * inner(dx_i, dx_j);  // (1/2) <dphi/dx | dphi/dx>
+                    }
+                
+                    double pot_energy = inner(functions[i], V * functions[j]); // <phi|V|phi>
+
+                    H(i, j) = kin_energy + pot_energy; // Hamiltonian matrix
+                }
+            }
+            std::cout << "H: \n" << H << std::endl;
+
+            // Calculate the Overlap matrix
+            overlap = matrix_inner(world, functions, functions);
+
+            // Calculate the Diagonal matrix
+            Tensor<T> U;
+            Tensor<T> evals;
+            // sygvp is a function to solve the generalized eigenvalue problem HU = SUW where S is the overlap matrix and W is the diagonal matrix of eigenvalues of H
+            // The eigenvalues are stored in evals and the eigenvectors are stored in U
+            sygvp(world, H, overlap, 1, U, evals);
+            
+            diag_matrix.fill(0.0);
+            for(int i = 0; i < num; i++) {
+                diag_matrix(i, i) = evals(i); // Set the diagonal elements
+            }
+
+            std::cout << "dia_matrix: \n" << diag_matrix << std::endl;
+
+            std::vector<Function<T, NDIM>> y;
+
+            // y = U * functions
+            y = transform(world, functions, U);
+            
+            // std::cout << "U matrix: \n" << U << std::endl;
+            // std::cout << "evals: \n" << evals << std::endl;
+            return std::make_pair(evals, y);
+        }
+
+
+        // Function to generate and solve for each energy level
+        Function<T, NDIM> generate_and_solve(World& world, Function<T, NDIM>& V, const Function<T, NDIM> guess_function, int N, const std::vector<Function<T, NDIM>>& prev_phi, int max_iter) {
+
+            // Create the initial guess wave function
+            Function<T, NDIM> phi = guess_function;
+
+            phi.scale(1.0/phi.norm2()); // phi *= 1.0/norm
+            double E = energy(world, phi, V);
+
+            NonlinearSolverND<NDIM> solver;
+            int count_shift = 0; // counter how often the potential was shifted
+
+            for(int iter = 0; iter <= max_iter; iter++) {
+                //char filename[256];
+                //snprintf(filename, 256, "phi-%1d.dat", N);
+                //plot1D(filename,phi);
+                
+                // Energy cant be positiv
+                // shift potential
+                double shift = 0.0;
+
+                if (E > 0) {
+                    shift = -1.2 * E;
+                    count_shift++;
+                }
+
+                Function<T, NDIM> Vphi = V*phi + shift*phi; 
+                Vphi.truncate();
+
+                double a = -2 * E + (-2) * shift;
+
+                SeparatedConvolution<T,NDIM> op = BSHOperator<NDIM>(world, sqrt(a), 0.01, 1e-7);  
+
+                Function<T, NDIM> r = phi + 2.0 * op(Vphi); // the residual
+                T err = r.norm2();
+
+                // Q = 1 - sum(|phi_prev><phi_prev|) = 1 - |phi_0><phi_0| - |phi_1><phi_1| - ...
+                // Q*|Phi> = |Phi> - |phi_0><phi_0|Phi> - |phi_1><phi_1|Phi> - ...
+
+                for (const auto& prev_phi : prev_phi) {
+                    phi -= inner(prev_phi, phi)*prev_phi; 
+                }
+                phi.scale(1.0/phi.norm2());
+
+                phi = solver.update(phi, r);
+
+                double norm = phi.norm2();
+                phi.scale(1.0/norm);  // phi *= 1.0/norm
+                E = energy(world,phi,V); 
+
+                if (world.rank() == 0)
+                    print("iteration", iter, "energy", E, "norm", norm, "error",err);
+
+                if (err < 5e-4) break;
+            }
+
+            char filename[256];
+            snprintf(filename, 256, "phi-%1d.dat", N);
+
+            if (NDIM == 1)
+                plot1D(filename, phi);
+            else if (NDIM == 2)
+                plot2D(filename,phi);
+
+            if (count_shift != 0) {
+                std::cout << "Potential was shifted " << count_shift << " times" << std::endl;
+            }
+
+            print("Final energy without shift: ", E);
+            return phi;
+        }
+
+};
+
+#endif
--- src/bachelorthesis/wavefunctions.cc
+++ src/bachelorthesis/wavefunctions.cc
@@ -7,7 +7,6 @@
 #include <madness/mra/legendre.h>
 #include <madness/mra/mra.h>
 #include <madness/mra/funcplot.h>
-#include <madness/mra/nonlinsol.h>
 #include <madness/mra/operator.h>
 #include <madness/mra/vmra.h>
 #include <madness/mra/derivative.h>
@@ -22,161 +21,10 @@
 #include "guesses.h"
 #include "taylorseries.h"
 #include "plot.h"
+#include "eigensolver.h"
 
 
 using namespace madness;
-
-// Function to calculate the energy
-template <typename T, std::size_t NDIM>
-double energy(World& world, const Function<T, NDIM>& phi, const Function<T, NDIM>& V) {
-    double potential_energy = inner(phi, V * phi); // <phi|Vphi> = <phi|V|phi>
-    double kinetic_energy = 0.0;
-
-    Derivative<T, NDIM> D = free_space_derivative<T, NDIM>(world, 0); // Derivative operator
-
-    Function<T, NDIM> dphi = D(phi);
-    kinetic_energy += 0.5 * inner(dphi, dphi);  // (1/2) <dphi/dx | dphi/dx>
-
-    double energy = kinetic_energy + potential_energy;
-    return energy;
-}
-
-
-// Function to calculate the Hamiltonian matrix, Overlap matrix and Diagonal matrix
-template <typename T, std::size_t NDIM>
-std::pair<Tensor<T>, std::vector<Function<T, NDIM>>> diagonalize(World &world, const std::vector<Function<T, NDIM>>& functions, const Function<T, NDIM>& V){
-    const int num = functions.size();
-    
-    auto H = Tensor<T>(num, num); // Hamiltonian matrix
-    auto overlap = Tensor<T>(num, num); // Overlap matrix
-    auto diag_matrix = Tensor<T>(num, num); // Diagonal matrix
-
-    // Calculate the Hamiltonian matrix
-    
-    for(int i = 0; i < num; i++) {
-        auto energy1 = energy(world, functions[i], V);
-        std::cout << energy1 << std::endl;
-        for(int j = 0; j < num; j++) {
-            double kin_energy = 0.0;
-            for (int axis = 0; axis < NDIM; axis++) {
-                Derivative<T, NDIM> D = free_space_derivative<T,NDIM>(world, axis); // Derivative operator
-
-                Function<T, NDIM> dx_i = D(functions[i]);
-                Function<T, NDIM> dx_j = D(functions[j]);
-
-                kin_energy += 0.5 * inner(dx_i, dx_j);  // (1/2) <dphi/dx | dphi/dx>
-            }
-        
-            double pot_energy = inner(functions[i], V * functions[j]); // <phi|V|phi>
-
-            H(i, j) = kin_energy + pot_energy; // Hamiltonian matrix
-        }
-    }
-    std::cout << "H: \n" << H << std::endl;
-
-    // Calculate the Overlap matrix
-    overlap = matrix_inner(world, functions, functions);
-
-    // Calculate the Diagonal matrix
-    Tensor<T> U;
-    Tensor<T> evals;
-    // sygvp is a function to solve the generalized eigenvalue problem HU = SUW where S is the overlap matrix and W is the diagonal matrix of eigenvalues of H
-    // The eigenvalues are stored in evals and the eigenvectors are stored in U
-    sygvp(world, H, overlap, 1, U, evals);
-    
-    diag_matrix.fill(0.0);
-    for(int i = 0; i < num; i++) {
-        diag_matrix(i, i) = evals(i); // Set the diagonal elements
-    }
-
-    std::cout << "dia_matrix: \n" << diag_matrix << std::endl;
-
-    std::vector<Function<T, NDIM>> y;
-
-    // y = U * functions
-    y = transform(world, functions, U);
-    
-    // std::cout << "U matrix: \n" << U << std::endl;
-    // std::cout << "evals: \n" << evals << std::endl;
-    return std::make_pair(evals, y);
-}
-
-
-// Function to generate and solve for each energy level
-template <typename T, std::size_t NDIM>
-Function<T, NDIM> generate_and_solve(World& world, Function<T, NDIM>& V, const Function<T, NDIM> guess_function, int N, const std::vector<Function<T, NDIM>>& prev_phi) {
-
-    // Create the initial guess wave function
-    Function<T, NDIM> phi = guess_function;
-
-    phi.scale(1.0/phi.norm2()); // phi *= 1.0/norm
-    double E = energy(world, phi, V);
-
-    NonlinearSolverND<NDIM> solver;
-    int count_shift = 0; // counter how often the potential was shifted
-
-    for(int iter = 0; iter <= 50; iter++) {
-        //char filename[256];
-        //snprintf(filename, 256, "phi-%1d.dat", N);
-        //plot1D(filename,phi);
-        
-        // Energy cant be positiv
-        // shift potential
-
-        // TODO: Shift Energy -1.2 * E
-        if (E > 0) {
-            V = V - DELTA;
-            E = energy(world, phi, V);
-            count_shift++;
-        }
-
-        Function<T, NDIM> Vphi = V*phi;
-        Vphi.truncate();
-        
-        SeparatedConvolution<T,NDIM> op = BSHOperator<NDIM>(world, sqrt(-2*E), 0.01, 1e-7);  
-
-        Function<T, NDIM> r = phi + 2.0 * op(Vphi); // the residual
-        T err = r.norm2();
-
-        // Q = 1 - sum(|phi_prev><phi_prev|) = 1 - |phi_0><phi_0| - |phi_1><phi_1| - ...
-        // Q*|Phi> = |Phi> - |phi_0><phi_0|Phi> - |phi_1><phi_1|Phi> - ...
-
-        for (const auto& prev_phi : prev_phi) {
-            phi -= inner(prev_phi, phi)*prev_phi; 
-        }
-        phi.scale(1.0/phi.norm2());
-
-        phi = solver.update(phi, r);
-
-        double norm = phi.norm2();
-        phi.scale(1.0/norm);  // phi *= 1.0/norm
-        E = energy(world,phi,V); 
-
-        if (world.rank() == 0)
-            print("iteration", iter, "energy", E, "norm", norm, "error",err);
-
-        if (err < 5e-4) break;
-
-        // TODO: SHIFT POTENTIAL BACK
-    }
-
-    char filename[256];
-    snprintf(filename, 256, "phi-%1d.dat", N);
-
-    if (NDIM == 1)
-        plot1D(filename, phi);
-    else if (NDIM == 2)
-        plot2D(filename,phi);
-
-    if (count_shift != 0) {
-        std::cout << "Potential was shifted " << count_shift << " times" << std::endl;
-        V = V + count_shift * DELTA;
-    }
-
-    print("Final energy without shift: ", E + DELTA);
-    return phi;
-}
-
 
 int main(int argc, char** argv) {
     // Initializing
@@ -190,6 +38,7 @@ int main(int argc, char** argv) {
     const double thresh = 1e-6; // Threshold
     // Number of levels // for harmonic oscillator: 10, for gaussian potential: 5, for double well potential: 4, for exponential potential: 5, for morse potential: 4
     constexpr int num_levels = 4;  
+    constexpr int max_iter = 100; // Maximum number of iterations
     constexpr int NDIM = 1; // Dimension
 
     //-------------------------------------------------------------------------------//
@@ -251,14 +100,14 @@ int main(int argc, char** argv) {
     // Create the guess generator 
 
     //HarmonicGuessGenerator<double, NDIM> harmonic_guess_generator(world);      // Harmonic guess generator for harmonic and gaussian potential
-    GuessGenerator<double, NDIM> guess_generator(world);              // Guess generator for all potentials
+    //GuessGenerator<double, NDIM> guess_generator(world);              // Guess generator for all potentials
 
     //-------------------------------------------------------------------------------//
 
     // Create the guesses
 
     //std::vector<Function<double,NDIM>> guesses = harmonic_guess_generator.create_guesses(num_levels);    // Create the guesses for harmonic potential
-    std::vector<Function<double,NDIM>> guesses = guess_generator.create_guesses(num_levels, V);   // Create the guesses for all potential
+    //std::vector<Function<double,NDIM>> guesses = guess_generator.create_guesses(num_levels, V);   // Create the guesses for all potential
 
     //-------------------------------------------------------------------------------//
 
@@ -268,6 +117,9 @@ int main(int argc, char** argv) {
     else if (NDIM == 2)
         plot2D("potential2D.dat", V);
 
+    
+    //--------------------------------------------------------------------------------//
+    /*
     // Plot guesses
     for (int i = 0; i < guesses.size(); i++) {
         char filename[512];
@@ -277,29 +129,21 @@ int main(int argc, char** argv) {
         else if (NDIM == 2)
             plot2D(filename, guesses[i]);
     }
-
+    */
     //--------------------------------------------------------------------------------//
     
-    std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> tmp = diagonalize(world, guesses, V);
-    std::vector<Function<double, NDIM>> diagonalized_guesses = tmp.second;
-    
-    // Solve for each energy level and store the eigenfunctions
-    std::vector<Function<double, NDIM>> eigenfunctions;
+    // Create Eigensolver
+    Eigensolver<double, NDIM> solver(world);
 
-    for (int i = 0; i < num_levels; i++) {
-        Function<double, NDIM> phi = generate_and_solve(world, V, diagonalized_guesses[i], i, eigenfunctions);
-        eigenfunctions.push_back(phi);
-    }
+    //-------------------------------------------------------------------------------//
 
-    std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> diagonalized = diagonalize(world, eigenfunctions, V);
-    std::cout << "Diagonalize" << std::endl;
-    std::vector<Function<double, NDIM>> y = diagonalized.second;
-    //auto evals = tmp.first;
+    std::vector<Function<double, NDIM>> y = solver.solve(V, num_levels, 100); // Solve the eigenvalue problem for potential V 
 
+    // plot the eigenfunctions
     for (int i = 0; i < num_levels; i++) {
         char filename[256];
         snprintf(filename, 256, "Psi_%1d.dat", i);
-        double en = energy(world, y[i], V);
+        double en = solver.energy(world, y[i], V);
         std::cout << "Energy: " << en << std::endl;
 
         // for hamonic oscillator
@@ -311,7 +155,6 @@ int main(int argc, char** argv) {
         }
         */
         
-      
         if (NDIM == 1)
         // for harmonic oscillator: 0.75 * y[i] + en for optical reasons 
         // before it was: y[i] + DELTA + en
@@ -320,10 +163,10 @@ int main(int argc, char** argv) {
             plot2D(filename, y[i]);
     }
 
-    //-------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------//
     
     // calculate the Taylor series of potential
-
+    /*
     TaylorSeriesGenerator<double, NDIM> taylor_series_generator(world);
     //Vector<double, NDIM> x0(0.0);
     Function<double, NDIM> approximation = taylor_series_generator.create_taylorseries(world, V, mu, 2);
@@ -334,7 +177,7 @@ int main(int argc, char** argv) {
         plot2D("approximation2D.dat", approximation);
 
     //std::cout << "Taylor series created" << std::endl;
-    /*
+    
     Function<double, NDIM> approximation1 = taylor_series_generator.create_taylorseries(world, V, mu1, 2);
 
     if (NDIM == 1)
@@ -359,39 +202,28 @@ int main(int argc, char** argv) {
     */
     
     //--------------------------------------------------------------------------------//
+    
     // create the guesses for the taylor series
     HarmonicGuessGenerator<double, NDIM> harmonic_guess_generator(world);
     std::vector<Function<double,NDIM>> approximation_guesses = harmonic_guess_generator.create_guesses(num_levels);
 
-// Plot guesses
-    for (int i = 0; i < guesses.size(); i++) {
+    // Plot guesses
+    for (int i = 0; i < approximation_guesses.size(); i++) {
         char filename[512];
         snprintf(filename, 512, "ga-%1d.dat", i);
         if (NDIM == 1)
-            plot1D(filename, guesses[i]);
+            plot1D(filename, approximation_guesses[i]);
         else if (NDIM == 2)
-            plot2D(filename, guesses[i]);
+            plot2D(filename, approximation_guesses[i]);
     }
-
-
-    // diagonalize the taylor series
-    std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> tmp2 = diagonalize(world, approximation_guesses, V);
-    std::vector<Function<double, NDIM>> approximation_guesses_diagonalized = tmp2.second;
-
-    std::vector<Function<double, NDIM>> approximation_eigenfunctions;
-
-    for (int i = 0; i < num_levels; i++) {
-        Function<double, NDIM> phi = generate_and_solve(world, V, approximation_guesses_diagonalized[i], i, approximation_eigenfunctions);
-        approximation_eigenfunctions.push_back(phi);
-    }
-
-    std::pair<Tensor<double>, std::vector<Function<double, NDIM>>> approximation_diagonalized = diagonalize(world, approximation_eigenfunctions, V);
-    std::vector<Function<double, NDIM>> approximation_y = approximation_diagonalized.second;
+    
+    
+    std::vector<Function<double, NDIM>> approximation_y = solver.solve(V, approximation_guesses, num_levels, 100); // Solve the eigenvalue problem for potential V
 
     for(int i = 0; i < num_levels; i++) {
         char filename[256];
         snprintf(filename, 256, "PsiApprox_%1d.dat", i);
-        double en = energy(world, approximation_y[i], approximation);
+        double en = solver.energy(world, approximation_y[i], V);
         std::cout << "Energy: " << en << std::endl;
 
         if (NDIM == 1)
